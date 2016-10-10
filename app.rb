e21356cce261f65b6b7c54cf5f80fdfa3e642841
Tom Nunn <t@tomnunn.uk>
2016-10-10 15:03:03 +0100

    create views directory and index.erb file for html

--- app.rb
+++ app.rb
@@ -13,5 +13,5 @@ get '/secret' do
 end
 
 get '/cat' do
-  "<img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png' alt='The Cat' style='border: 8px solid tomato'>"
+  erb(:index)
 end
