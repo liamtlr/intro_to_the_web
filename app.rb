b835bf683d6e960aafbac5ea0b1c79edc9664d83
Liam Taylor <liam.taylor888@googlemail.com>
2016-10-10 14:41:41 +0100

    Tidying up app.rb'

--- app.rb
+++ app.rb
@@ -11,13 +11,6 @@ get '/secret' do
   "Ello ello"
 end
 
-get '/page1' do
-  "Hilarious team name"
-end
-
-get '/hungry' do
-  "AM STILL UNGREH"
-end
 
 get '/cat' do
 "<img src='http://bit.ly/1eze8aE' alt='cat' style='border: 10px solid tomato'>"
